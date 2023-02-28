9e37162759a8b1797d6069d95ac49123ad0f9146
phil_litian <phil_litian@163.com>
2023-02-28 22:31:31 +0800

    feat: base loader

--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
-console.log('sum');
+import './style/index.css'
+import './style/index.less'
+import './style/index.scss'
+import './style/index.sass'
--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,29 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: resolve(__dirname, 'dist'),
-    filename: 'boundle.js',
+    filename: 'boundle.js'
   },
-  // modules: [],
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      { test: /\.less$/i, use: ['style-loader', 'css-loader', 'less-loader'] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.png|jpe?g|gif|webp$/i,
+        type: 'asset', // webpack5默认对图片资源进行处理
+        parser: {
+          dataUrlCondition: {
+            // 一般限制10kb以内使用base64， 10kb以上还是使用url加载图片资源
+            maxSize: 10 * 1024 // 10kb
+          }
+        }
+      }
+    ]
+  },
+  // 插件
   // plugins: [],
   mode: 'development'
 }
